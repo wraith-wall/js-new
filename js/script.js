041e91f9809ed809c82b76d5f948f4cc0aba88b0
reyghery246t4wy3we <soba4kato4kacom@gmail.com>
2024-11-23 22:44:31 +0200

    create db

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log(1);
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	private: false,
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+	b = prompt("На сколько оцените его?", ""),
+	c = prompt("Один из последних просмотренных фильмов?", ""),
+	d = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
